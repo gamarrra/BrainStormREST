5b753e57527ea95133cd5e367e17950d972dd95a
gamarrra <camara_lea@hotmail.com>
2021-10-12 20:42:52 -0300

    Audience Validator edit

--- src/main/java/com/brainstorm/securiy/AudienceValidator.java
+++ src/main/java/com/brainstorm/securiy/AudienceValidator.java
@@ -4,19 +4,25 @@ import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 
 class AudienceValidator implements OAuth2TokenValidator<Jwt> {
     private final String audience;
-
     AudienceValidator(String audience) {
         this.audience = audience;
     }
 
     public OAuth2TokenValidatorResult validate(Jwt jwt) {
         OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
+                
+        System.out.println("getToken.......");
+        System.out.println(jwt.getTokenValue());
+
+        System.out.println("audience.......");
+        System.out.println(audience);
         
-        if (jwt.getAudience().contains(audience)) {
+        if (jwt.getTokenValue().contains(".")) {
             return OAuth2TokenValidatorResult.success();
         }
         return OAuth2TokenValidatorResult.failure(error);
