d827711bf0e4ee2a4cc4cd93755416b55cb6b902
gamarrra <camara_lea@hotmail.com>
2021-11-09 23:09:58 -0300

    ...

--- src/main/java/com/brainstorm/repository/TareasRepository.java
+++ src/main/java/com/brainstorm/repository/TareasRepository.java
@@ -4,18 +4,24 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.brainstorm.model.Tarea;
+import com.brainstorm.model.Usuario;
 
 
 
 @Repository
 public interface TareasRepository extends JpaRepository<Tarea, Long> {
+	
 	@Query(value = "select * from tareas",nativeQuery=true)
 	List <Tarea> SearchAll();
 	
 	@Query(value = "select * from tareas inner join grupos on tareas.grupo_grupo_id = grupos.grupo_id;", nativeQuery=true)
 	List <Tarea> GetTaskGroup();
+	
+	@Query("select s from Tarea s where s.grupoCreador.getGrupoId = :grupoCreador")
+	List<Tarea> findByGrupoCreador(@Param("grupoCreador") Long grupoCreador);
 }
 
